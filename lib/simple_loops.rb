3ecddd0d1e960234e4c265f40f38ed1cd55de579
Ryan Locascio <loc.fitness1@gmail.com>
2020-06-18 15:38:13 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,4 +4,12 @@ def loop_message_five_times(message)
     puts message
     count += 1
   end
-end 
+end
+
+def loop_message_n_times(messsage, interger)
+    count = 0
+  while count == interger
+    puts message
+    counter += 1
+  end
+end
